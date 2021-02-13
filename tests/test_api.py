9bf766c5d97cbc404b31b779682dcc0aa08a03a9
Jan Gosmann <jan@hyper-world.de>
2021-02-13 16:51:45 +0100

    Handle constant shapeclass field

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,12 @@ def test_required_field():
         TestData()
 
     assert TestData(field="foo").field == "foo"
+
+
+def test_constant_field():
+    @shapeclass
+    class TestData:
+        field: str = "value"
+
+    assert TestData().field == "value"
+    assert TestData("different value").field == "different value"
