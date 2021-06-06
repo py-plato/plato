2223c894a227035ef22b01a928f08695add87061
Jan Gosmann <jan@hyper-world.de>
2021-06-06 11:19:38 +0200

    chore: Enable pylint for tests folder

--- tests/internal/test_weak_id_dict.py
+++ tests/internal/test_weak_id_dict.py
@@ -10,64 +10,64 @@ class Dummy:
 
 
 def test_missing_key():
-    d = WeakIdDict()
+    weak_id_dict = WeakIdDict()
     key = Dummy()
 
-    assert key not in d
+    assert key not in weak_id_dict
     with pytest.raises(KeyError):
-        d[key]
+        weak_id_dict[key]  # pylint: disable=pointless-statement
 
     with pytest.raises(KeyError):
-        del d[key]
+        del weak_id_dict[key]
 
 
 def test_setting_key():
-    d = WeakIdDict()
+    weak_id_dict = WeakIdDict()
     key = Dummy()
 
-    d[key] = "value"
-    assert key in d
-    assert d[key] == "value"
+    weak_id_dict[key] = "value"
+    assert key in weak_id_dict
+    assert weak_id_dict[key] == "value"
 
-    d[key] = "different-value"
-    assert key in d
-    assert d[key] == "different-value"
+    weak_id_dict[key] = "different-value"
+    assert key in weak_id_dict
+    assert weak_id_dict[key] == "different-value"
 
     different_key = Dummy()
-    assert different_key not in d
-    d[different_key] = "another-value"
-    assert d[key] == "different-value"
+    assert different_key not in weak_id_dict
+    weak_id_dict[different_key] = "another-value"
+    assert weak_id_dict[key] == "different-value"
 
 
 def test_deleting_key():
-    d = WeakIdDict()
+    weak_id_dict = WeakIdDict()
     key = Dummy()
 
-    d[key] = "value"
-    del d[key]
+    weak_id_dict[key] = "value"
+    del weak_id_dict[key]
 
-    assert key not in d
+    assert key not in weak_id_dict
     with pytest.raises(KeyError):
-        d[key]
+        weak_id_dict[key]  # pylint: disable=pointless-statement
 
 
 def test_iterating_keys():
-    d = WeakIdDict()
+    weak_id_dict = WeakIdDict()
     keys = [Dummy() for i in range(4)]
 
     for k in keys:
-        d[k] = "value"
+        weak_id_dict[k] = "value"
 
-    assert list(d) == keys
-    assert len(d) == len(keys)
+    assert list(weak_id_dict) == keys
+    assert len(weak_id_dict) == len(keys)
 
 
 def test_keys_are_weak():
-    d = WeakIdDict()
+    weak_id_dict = WeakIdDict()
     key = Dummy()
     ref = weakref.ref(key)
 
-    d[key] = "value"
+    weak_id_dict[key] = "value"
     del key
 
     assert ref() is None
